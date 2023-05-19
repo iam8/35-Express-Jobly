e83f1a74972413643df25fc22b573ee78e52fb2a
alexm <alex-m@outlook.com>
2023-05-19 13:18:04 -0700

    Import and use hostname variable from config file

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const app = require("./app");
-const { PORT } = require("./config");
+const { HOSTNAME, PORT } = require("./config");
 
-app.listen(PORT, function () {
-  console.log(`Started on http://localhost:${PORT}`);
+app.listen(PORT, HOSTNAME, function () {
+    console.log(`Started on http://${HOSTNAME}:${PORT}`);
 });
