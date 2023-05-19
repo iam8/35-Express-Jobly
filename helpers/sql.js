05a1bbb4de6b2268e145473ea01e3f0df9c3e36f
alexm <alex-m@outlook.com>
2023-05-19 14:51:01 -0700

    Add docstring and improved comment in sqlForPartialUpdate

--- helpers/sql.js
+++ helpers/sql.js
@@ -8,15 +8,28 @@
 
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
 /**
- * Add docstring here.
+ * Create and return an object containing a string of column names to update, and an array of
+ * new values for those columns.
+ *
+ * Throw a BadRequestError if the given dataToUpdate contains no entries.
+ *
+ * Params:
+ * - dataToUpdate (object) - data to update. Keys are column names to update, values are the new
+ * values.
+ * - jsToSql (object) - mapping of JS property names to their corresponding SQL column names.
+ *
+ * Returned object properties:
+ * - setCols: prepared SQL statement for updating data, separated by commas (i.e. "col1=$1,
+ * col2=$2, col3=$3")
+ * - values: array of new values in corresponding order (i.e. [value1, value2, value3])
  */
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
     const keys = Object.keys(dataToUpdate);
     if (keys.length === 0) throw new BadRequestError("No data");
 
-    // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+    // Create prepared statement for SQL.
+    // Example: keys ['firstName', 'age'] => ['"first_name"=$1', '"age"=$2']
     const cols = keys.map((colName, idx) =>
         `"${jsToSql[colName] || colName}"=$${idx + 1}`,
     );
