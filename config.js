adb93b80f7f9f1169fa8a740015f0bf51b1213da
alexm <alex-m@outlook.com>
2023-05-19 13:17:44 -0700

    Add hostname variable and export it

--- config.js
+++ config.js
@@ -7,13 +7,14 @@ require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
+const HOSTNAME = process.env.HOSTNAME || "127.0.0.1";
 const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
-  return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+    return (process.env.NODE_ENV === "test")
+        ? "jobly_test"
+        : process.env.DATABASE_URL || "jobly";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
@@ -29,8 +30,9 @@ console.log("Database:".yellow, getDatabaseUri());
 console.log("---");
 
 module.exports = {
-  SECRET_KEY,
-  PORT,
-  BCRYPT_WORK_FACTOR,
-  getDatabaseUri,
+    SECRET_KEY,
+    HOSTNAME,
+    PORT,
+    BCRYPT_WORK_FACTOR,
+    getDatabaseUri,
 };
