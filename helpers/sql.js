131fa5e9884c0e918cbbb2224e44197177042a8b
alexm <alex-m@outlook.com>
2023-05-19 14:32:31 -0700

    Adjust indentation and spacing; add my header and module docstring

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,20 +1,33 @@
+// Ioana A Mititean
+// Unit 35: Jobly
+
+/**
+ * Helpers for writing SQL queries.
+ */
+
+
 const { BadRequestError } = require("../expressError");
 
 // THIS NEEDS SOME GREAT DOCUMENTATION.
-
+/**
+ * Add docstring here.
+ */
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
-  const keys = Object.keys(dataToUpdate);
-  if (keys.length === 0) throw new BadRequestError("No data");
+    const keys = Object.keys(dataToUpdate);
+    if (keys.length === 0) throw new BadRequestError("No data");
 
-  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-  const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
-  );
+    // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+    const cols = keys.map((colName, idx) =>
+        `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+    );
 
-  return {
-    setCols: cols.join(", "),
-    values: Object.values(dataToUpdate),
-  };
+    return {
+        setCols: cols.join(", "),
+        values: Object.values(dataToUpdate),
+    };
 }
 
-module.exports = { sqlForPartialUpdate };
+
+module.exports = {
+    sqlForPartialUpdate
+};
