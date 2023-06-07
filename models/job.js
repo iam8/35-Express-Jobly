6592217a700571e3aec62551358560867b29eb33
alexm <alex-m@outlook.com>
2023-06-07 13:25:06 -0700

    Add code to remove() method

--- models/job.js
+++ models/job.js
@@ -131,7 +131,16 @@ class Job {
      * Throws NotFoundError if job not found.
      */
     static async remove(id) {
+        const result = await db.query(
+            `DELETE
+             FROM jobs
+             WHERE id = $1
+             RETURNING id`,
+            [id]);
 
+      const job = result.rows[0];
+
+      if (!job) throw new NotFoundError(`No job found: ${id}`);
     }
 }
 
