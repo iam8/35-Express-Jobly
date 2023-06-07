c25ef02d737ff84777e35c6c23d852e0eca3fe8b
alexm <alex-m@outlook.com>
2023-06-07 13:05:01 -0700

    Add code to create() method

--- models/job.js
+++ models/job.js
@@ -26,7 +26,27 @@ class Job {
      * Throws BadRequestError if job is already in database.
      */
     static async create({title, salary, equity, company_handle}) {
+        const duplicateCheck = await db.query(`
+            SELECT title FROM jobs
+            WHERE title = $1`,
+            [title]
+        );
 
+        if (duplicateCheck.rows[0])
+            throw new BadRequestError(`Duplicate job: '${title}'`);
+
+        const result = await db.query(`
+            INSERT INTO jobs
+                (title, salary, equity, company_handle)
+            VALUES
+                ($1, $2, $3, $4)
+            RETURNING
+                (id, title, salary, equity, company_handle AS "companyHandle")`,
+            [title, salary, equity, company_handle]
+        );
+
+        const job = result.rows[0];
+        return job;
     }
 
     /**
