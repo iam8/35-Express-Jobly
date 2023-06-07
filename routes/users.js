bc7283d4a83f1e510811f7e8816f7b8b8cc60336
alexm <alex-m@outlook.com>
2023-06-06 19:14:09 -0700

    Import and use new middleware in GET /users/:username route; remove old code added in this route with same functionality

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@
 const jsonschema = require("jsonschema");
 
 const express = require("express");
-const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
+const { ensureLoggedIn, ensureAdmin, ensureAdminOrSpecificUser} = require("../middleware/auth");
 const { BadRequestError } = require("../expressError");
 const { UnauthorizedError } = require("../expressError");
 const User = require("../models/user");
@@ -69,15 +69,9 @@ router.get("/", ensureAdmin, async function (req, res, next) {
  * Authorization required: login, admin or corresponding user
  **/
 
-router.get("/:username", ensureLoggedIn, async function (req, res, next) {
+router.get("/:username", ensureAdminOrSpecificUser, async function (req, res, next) {
   try {
     const user = await User.get(req.params.username);
-
-    // Check if user is admin or corresponding user
-    if (!res.locals.user.isAdmin && res.locals.user.username !== req.params.username) {
-        throw new UnauthorizedError();
-    }
-
     return res.json({ user });
   } catch (err) {
     return next(err);
