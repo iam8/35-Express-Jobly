a77ebe4ee603bb5e81b1f04849b8b7a140cfa058
alexm <alex-m@outlook.com>
2023-06-07 12:45:21 -0700

    Add imports and exports; add stubs and docstrings for Job class functions

--- models/job.js
+++ models/job.js
@@ -5,3 +5,54 @@
  * Model for jobs.
  */
 
+"use strict";
+
+const db = require("../db");
+const { BadRequestError, NotFoundError } = require("../expressError");
+
+
+/**
+ * Related functions for jobs.
+ */
+class Job {
+
+    /**
+     * Create a new job.
+     */
+    static async create({}) {
+
+    }
+
+    /**
+     * Find all jobs.
+     */
+    static async findAll(filters) {
+
+    }
+
+    /**
+     * Given a job title, return info about that job.
+     */
+    static async get(title) {
+
+    }
+
+    /**
+     * Update job with 'data'.
+     */
+    static async update(title, data) {
+
+    }
+
+    /**
+     * Delete a job.
+     */
+    static async remove(title) {
+
+    }
+}
+
+
+module.exports = {
+    Job
+};
