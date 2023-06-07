4392106051943c2f1c7890b38a35aaf21a99b45b
alexm <alex-m@outlook.com>
2023-06-07 12:55:40 -0700

    Complete class method docstrings; change method parameters to reflect job ID instead of title; add parameters to create() definition

--- models/job.js
+++ models/job.js
@@ -17,37 +17,64 @@ const { BadRequestError, NotFoundError } = require("../expressError");
 class Job {
 
     /**
-     * Create a new job.
+     * Create a new job from given data, update database, and return the new job data.
+     *
+     * Data format: {title, salary, equity, company_handle}
+     *
+     * Returns: {id, title, salary, equity, company_handle}
+     *
+     * Throws BadRequestError if job is already in database.
      */
-    static async create({}) {
+    static async create({title, salary, equity, company_handle}) {
 
     }
 
     /**
      * Find all jobs.
+     *
+     * TODO: filtering.
+     *
+     * Returns: [{id, title, salary, equity, company_handle}, ...]
      */
     static async findAll(filters) {
 
     }
 
     /**
-     * Given a job title, return info about that job.
+     * Given a job ID, return data about that job.
+     *
+     * Returns {id, title, salary, equity, company_handle}
+     *
+     * Throws NotFoundError if job not found.
      */
-    static async get(title) {
+    static async get(id) {
 
     }
 
     /**
-     * Update job with 'data'.
+     * Update job data with 'data'.
+     *
+     * This is a "partial update" --- it's fine if data doesn't contain all the fields; this only
+     * changes provided ones.
+     *
+     * Data can include: {title, salary, equity}
+     *
+     * Returns {id, title, salary, equity, company_handle}
+     *
+     * Throws NotFoundError if job not found.
      */
-    static async update(title, data) {
+    static async update(id, data) {
 
     }
 
     /**
-     * Delete a job.
+     * Delete a job (by ID).
+     *
+     * Returns undefined.
+     *
+     * Throws NotFoundError if job not found.
      */
-    static async remove(title) {
+    static async remove(id) {
 
     }
 }
