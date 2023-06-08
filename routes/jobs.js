33c4eab32996e625fb168bbc602c21277542c362
alexm <alex-m@outlook.com>
2023-06-08 13:28:24 -0700

    Remove unused import; import job schemas

--- routes/jobs.js
+++ routes/jobs.js
@@ -13,8 +13,11 @@ const jsonschema = require("jsonschema");
 const express = require("express");
 
 const { BadRequestError } = require("../expressError");
-const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
+const { ensureAdmin } = require("../middleware/auth");
 const { Job } = require("../models/job");
+
+const jobNewSchema = require("../schemas/jobNew.json");
+const jobUpdateSchema = require("../schemas/jobUpdate.json");
 
 const router = new express.Router();
 
