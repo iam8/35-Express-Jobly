3b9a317a796676a8036a05cb666534e782fe400a
alexm <alex-m@outlook.com>
2023-06-08 11:55:25 -0700

    Adjust test expectation for GET /companies/:handle according to specifications for companies with no jobs

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -216,6 +216,7 @@ describe("GET /companies/:handle", function () {
         description: "Desc2",
         numEmployees: 2,
         logoUrl: "http://c2.img",
+        jobs: []
       },
     });
   });
