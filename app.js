98b259549d397e24013105c109f49de54447df87
alexm <alex-m@outlook.com>
2023-06-10 13:42:33 -0700

    Adjust indentation and spacing; adjust order of imports

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+// Ioana A Mititean
+// Unit 35 - Jobly
+
 "use strict";
 
 /** Express app for jobly. */
 
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const { NotFoundError } = require("./expressError");
-
 const { authenticateJWT } = require("./middleware/auth");
 const authRoutes = require("./routes/auth");
 const companiesRoutes = require("./routes/companies");
 const usersRoutes = require("./routes/users");
 const {router: jobsRoutes} = require("./routes/jobs");
 
-const morgan = require("morgan");
 
 const app = express();
 
@@ -30,18 +32,20 @@ app.use("/jobs", jobsRoutes);
 
 /** Handle 404 errors -- this matches everything */
 app.use(function (req, res, next) {
-  return next(new NotFoundError());
+    return next(new NotFoundError());
 });
+
 
 /** Generic error handler; anything unhandled goes here. */
 app.use(function (err, req, res, next) {
-  if (process.env.NODE_ENV !== "test") console.error(err.stack);
-  const status = err.status || 500;
-  const message = err.message;
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
 
-  return res.status(status).json({
-    error: { message, status },
-  });
+    return res.status(status).json({
+        error: { message, status },
+    });
 });
+
 
 module.exports = app;
