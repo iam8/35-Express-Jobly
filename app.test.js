a54c0c05ef8eedf5951f017f101d8e49147a2a67
alexm <alex-m@outlook.com>
2023-06-10 13:46:54 -0700

    Adjust indentation and spacing; add my header to top

--- app.test.js
+++ app.test.js
@@ -1,3 +1,6 @@
+// Ioana A Mititean
+// Unit 35 - Jobly
+
 const request = require("supertest");
 
 const app = require("./app");
@@ -5,17 +8,19 @@ const db = require("./db");
 
 
 test("not found for site 404", async function () {
-  const resp = await request(app).get("/no-such-path");
-  expect(resp.statusCode).toEqual(404);
+    const resp = await request(app).get("/no-such-path");
+    expect(resp.statusCode).toEqual(404);
 });
+
 
 test("not found for site 404 (test stack print)", async function () {
-  process.env.NODE_ENV = "";
-  const resp = await request(app).get("/no-such-path");
-  expect(resp.statusCode).toEqual(404);
-  delete process.env.NODE_ENV;
+    process.env.NODE_ENV = "";
+    const resp = await request(app).get("/no-such-path");
+    expect(resp.statusCode).toEqual(404);
+    delete process.env.NODE_ENV;
 });
 
+
 afterAll(function () {
-  db.end();
+    db.end();
 });
