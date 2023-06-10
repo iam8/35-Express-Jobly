b7fc9bd331a924866fdc4021bd5fe70dd9c9b789
alexm <alex-m@outlook.com>
2023-06-10 13:52:42 -0700

    Adjust indentation and spacing; add my header to top

--- db.js
+++ db.js
@@ -1,4 +1,9 @@
+// Ioana A Mititean
+// Unit 35 - Jobly
+
 "use strict";
+
+
 /** Database setup for jobly. */
 const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
@@ -6,16 +11,16 @@ const { getDatabaseUri } = require("./config");
 let db;
 
 if (process.env.NODE_ENV === "production") {
-  db = new Client({
-    connectionString: getDatabaseUri(),
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
+    db = new Client({
+        connectionString: getDatabaseUri(),
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
 } else {
-  db = new Client({
-    connectionString: getDatabaseUri()
-  });
+    db = new Client({
+        connectionString: getDatabaseUri()
+    });
 }
 
 db.connect();
