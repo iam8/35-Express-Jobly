58f377f9b750f8c818e0741618392fb8c926aba6
alexm <alex-m@outlook.com>
2023-06-10 14:01:19 -0700

    Add module docstring

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+// Ioana A Mititean
+// Unit 35 - Jobly
+
+/**
+ * Start the Jobly app on the server with port and hostname defined in config file.
+ */
+
 "use strict";
+
 
 const app = require("./app");
 const { HOSTNAME, PORT } = require("./config");
