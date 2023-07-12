8f8c287b339106c857a57b819a6544712a01f35d
alexm <alex-m@outlook.com>
2023-07-11 17:12:22 -0700

    Remove some line comments

--- helpers/sql.js
+++ helpers/sql.js
@@ -28,8 +28,6 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
     const keys = Object.keys(dataToUpdate);
     if (keys.length === 0) throw new BadRequestError("No data");
 
-    // Create prepared statement for SQL.
-    // Example: keys ['firstName', 'age'] => ['"first_name"=$1', '"age"=$2']
     const cols = keys.map((colName, idx) =>
         `"${jsToSql[colName] || colName}"=$${idx + 1}`,
     );
